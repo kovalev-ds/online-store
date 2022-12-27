e93a91e910345be1a61220a3fb070b7fa9620b0d
Dima <dimaberry1@gmail.com>
2022-12-27 15:04:16 +0300

    feat: add style on filter components

--- src/components/FilterControl.tsx
+++ src/components/FilterControl.tsx
@@ -3,8 +3,8 @@ import { FC, useEffect, useState } from "react";
 type FilterControlProps = {
   value: string;
   selected: boolean;
-  handle: (checked: boolean) => void
-}
+  handle: (checked: boolean) => void;
+};
 
 const FilterControl: FC<FilterControlProps> = (props) => {
   const { value, handle, selected } = props;
@@ -13,15 +13,16 @@ const FilterControl: FC<FilterControlProps> = (props) => {
     <div>
       <label>
         <input
+          className="h-3 w-3 border border-gray-300 rounded-sm bg-white checked:bg-gray-600 checked:border-gray-600 focus:ring-white transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left ml-2 mr-2 mt-2 cursor-pointer "
           type="checkbox"
           value={value}
           checked={selected}
           onChange={(e) => handle(e.target.checked)}
         />
-        <span>{value}</span>
+        <span className="text-xs">{value}</span>
       </label>
     </div>
-  )
-}
+  );
+};
 
 export default FilterControl;

0e8c5ce05a26ce2200d4b8aa3b0351220222a132
Dima <dimaberry1@gmail.com>
2022-12-28 23:18:30 +0300

    feat: add new styles in buy-form

--- src/components/BuyForm.tsx
+++ src/components/BuyForm.tsx
@@ -2,7 +2,7 @@ const BuyForm = () => {
   return (
     <form
       action=""
-      className="min-w-[440px] min-h-[560px] bg-yellow-100 rounded-2xl border border-black border-opacity-10 shadow flex flex-col items-center"
+      className="min-w-[440px] min-h-[560px] bg-pink-100 rounded-2xl border border-black border-opacity-10 shadow flex flex-col items-center"
     >
       <label htmlFor="" className="mt-2 mb-2">
         Personal details
@@ -10,35 +10,35 @@ const BuyForm = () => {
       <input
         type="text"
         placeholder="Name"
-        className="bg-yellow-200 w-4/5 rounded-md mb-4"
+        className="bg-white w-4/5 rounded-md mb-4 text-center"
       />
       <input
         type="tel"
         placeholder="Phone number"
-        className="bg-yellow-200 w-4/5 rounded-md mb-4"
+        className="bg-white w-4/5 rounded-md mb-4 text-center"
       />
       <input
         type="text"
         placeholder="Delivery address"
-        className="bg-yellow-200 w-4/5 rounded-md mb-4"
+        className="bg-white w-4/5 rounded-md mb-4 text-center"
       />
       <input
         type="email"
         placeholder="E-mail"
-        className="bg-yellow-200 w-4/5 rounded-md mb-4"
+        className="bg-white w-4/5 rounded-md mb-4 text-center"
       />
       <label htmlFor="" className="mb-2">
         Credit card details
       </label>
-      <div className="w-3/4 h-40 bg-green-600 rounded-xl mb-6">
+      <div className="w-3/4 h-40 bg-gradient-to-r from-gray-500 to-gray-300  rounded-xl mb-6">
         <div className="flex">
-          <div className="min-w-[40px] h-8 bg-yellow-200 rounded-md mt-6 mx-2 flex items-center justify-center">
+          <div className="min-w-[40px] h-8 bg-white rounded-md mt-6 mx-2 flex items-center justify-center">
             <span>logo</span>
           </div>
           <input
             type="number"
             placeholder="Card number"
-            className="bg-yellow-200 w-4/5 h-8 rounded-md mt-6 mb-4"
+            className="bg-white w-4/5 h-8 rounded-md mt-6 mb-4 text-center"
           />
         </div>
         <div className="flex items-center justify-center">
@@ -48,7 +48,7 @@ const BuyForm = () => {
           <input
             type="namber"
             placeholder="Valid Thru"
-            className="bg-yellow-200 w-[100px] h-6 rounded-md mt-6 mb-4"
+            className="bg-white w-[100px] h-6 rounded-md mt-6 mb-4 text-center"
           />
           <label htmlFor="" className="mt-2 mr-1 ml-4">
             CVV:
@@ -56,7 +56,7 @@ const BuyForm = () => {
           <input
             type="number"
             placeholder="Code"
-            className="bg-yellow-200 w-[80px] h-6 rounded-md mt-6 mb-4"
+            className="bg-white w-[80px] h-6 rounded-md mt-6 mb-4 text-center"
           />
         </div>
       </div>

7e08a9b4ae4fd08d74ec099b3a501dc49a9316f9
Dima <dimaberry1@gmail.com>
2022-12-30 00:36:57 +0300

    feat: add img and styles in product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,11 +5,22 @@ import { Product } from '../types';
 const ProductPage = () => {
   const product = useLoaderData() as Product;
 
-  console.log(product);
+  // console.log(product);
+  const mainImage = (
+    <img src={product.images[0]} className="max-w-[300px] m-2 rounded-sm" alt="product foto" />
+  );
 
   const images = product.images.map((e) => (
-    <img className="max-w-[150px]" src={e} alt="product foto" key={e.toString()} />
+    <img
+      className="max-w-[100px] m-2 rounded-sm"
+      src={e}
+      alt="product foto"
+      key={e.toString()}
+      // onClick={() => (mainImage = <img src={e} alt="product foto" />)}
+    />
   ));
+
+  // console.log(mainImage.props.src);
 
   return (
     <div className="container">
@@ -19,20 +30,40 @@ const ProductPage = () => {
           <h1 className="py-2">{product.title}</h1>
         </div>
         <div className="flex">
-          <div className="">{images}</div>
-          {/* <div>main img</div> */}
-          <div className="flex flex-col">
-            <h2>{product.description}</h2>
-            <h2>{product.discountPercentage}</h2>
-            <h2>{product.rating}</h2>
-            <h2>{product.stock}</h2>
-            <h2>{product.brand}</h2>
-            <h2>{product.category}</h2>
+          <div className="bg-black">{images}</div>
+          <div className="bg-black flex justify-center items-center">{mainImage}</div>
+          <div className="flex flex-col bg-red-300 w-8/12">
+            <h2 className="bg-blue-500 mt-4 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Description:</p>
+              {product.description}
+            </h2>
+            <h2 className="bg-blue-500 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Discount Percentage:</p>
+              {product.discountPercentage}
+            </h2>
+            <h2 className="bg-blue-500 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Rating:</p>
+              {product.rating}
+            </h2>
+            <h2 className="bg-blue-500 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Stock:</p>
+              {product.stock}
+            </h2>
+            <h2 className="bg-blue-500 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Brand:</p>
+              {product.brand}
+            </h2>
+            <h2 className="bg-blue-500 mb-4 m-2 rounded-sm text-center">
+              <p className="bg-white rounded-t-sm">Category:</p>
+              {product.category}
+            </h2>
           </div>
-          <div>
-            <h2>{product.price}$</h2>
-            <button>Add To Cart {/* состояние корзины */}</button>
-            <button>BUY NOW</button>
+          <div className="bg-blue-500 flex justify-center items-center flex-col w-4/12">
+            <h2 className="text-3xl">{product.price}$</h2>
+            <button className="bg-white m-4 w-4/6 rounded-sm">
+              Add To Cart {/* состояние корзины */}
+            </button>
+            <button className="bg-white w-4/6 rounded-sm">BUY NOW</button>
           </div>
         </div>
       </div>
